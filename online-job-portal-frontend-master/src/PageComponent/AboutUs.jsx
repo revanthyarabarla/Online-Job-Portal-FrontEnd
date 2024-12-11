9a89492d92dad60e1f128fa8d3b757ca183b9561
REVANTH YARABARLA <112111893+revanthyarabarla@users.noreply.github.com>
2024-12-11 19:35:13 +0530

    Add files via upload

---
+++ online-job-portal-frontend-master/src/PageComponent/AboutUs.jsx
@@ -0,0 +1,27 @@
+const AboutUs = () => {
+  return (
+    <div className="text-color ms-5 me-5 mr-5 mt-3">
+      <h4 className="mb-3">About Us</h4>
+      <b>
+      Welcome to Online Job Portal, your ultimate destination for connecting job seekers with top employers.
+       We are dedicated to simplifying the recruitment process, making it easier than ever for professionals to 
+       find their dream jobs and for companies to discover exceptional talent.
+        <br />
+        <br />
+        Online Job Portal bridges the gap between employers and candidates, offering a comprehensive platform that 
+        serves both Business-to-Customer (B2C) and Business-to-Business (B2B) needs. Employers can showcase their 
+        opportunities and attract qualified candidates, while job seekers can explore a wide range of openings, 
+        submit applications, and build their professional journey.
+        <br />
+        <br />
+        At Online Job Portal, we believe that finding the right job or hiring the ideal candidate should never 
+        feel overwhelming. Our platform is built with user-friendly features, advanced search options, and intelligent 
+        matching algorithms to ensure that the process is as smooth and tailored as possible. With a commitment to 
+        innovation and excellence, we continuously strive to enhance our services, empowering individuals to achieve 
+        their career goals and organizations to thrive with the right talent.
+      </b>
+    </div>
+  );
+};
+
+export default AboutUs;
